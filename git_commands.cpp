df2532858632de6b6be95683e846cdb78dfcc749
Sanket Waghmode <sanketwaghmode99@gmail.com>
2021-02-16 13:58:35 +0530

    commiting in new feature with git commands

---
+++ git_commands.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+1. git branch - to check avaulable branhes on local repo 
+2. git checkout branch_name - to jump on branch branch_name
+3. 
+
+int main()
+{ 
+
+	cout<<" to "
+	return 0;
+}
--- world.cpp
+++ world.cpp
@@ -1,3 +1,33 @@
-its newly created file
-
-may be i am in new feature
+#include <bits/stdc++.h>
+using namespace std;
+int getnumber(string s)
+{
+    if(s=="12:00 AM")
+    return 0;
+    if(s[0]=='1' && s[1]=='2' && s[6]=='P')
+    {
+        return 1200+(((int)(s[3]))*10)+(int)(s[4]);
+    }
+    
+    int ans=0;
+    if(s[0]=='1')
+    ans=10;
+    ans+=(int)(s[1]);
+    
+    ans*=10;
+    ans+=(int)(s[3]);
+    ans*=10;
+    ans+=(int)(s[4]);
+    if(s[6]=='A')
+    return ans;
+    else
+    return ans+1200;
+}
+int main()
+{
+	//12:00 AM 11:42 PM
+	string s="12:02 AM";
+	int num=getnumber(s);
+	cout<<num;
+	return 0;
+}
