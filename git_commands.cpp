e8fd991230389e434e64faf5eb4b774a22778a3b
Sanket Waghmode <sanketwaghmode99@gmail.com>
2021-02-16 14:07:51 +0530

    commited commands in master

---
+++ git_commands.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+1. git branch - to check avaulable branhes on local repo 
+2. git checkout branch_name - to jump on branch branch_name
+3. git push :
+       git push- pushes current branch of local repo to remote repo
+       git push -u origin master_branch_name - when first time pushing ur local repo
+       
+
+int main()
+{ 
+
+	cout<<" to "
+	return 0;
+}
